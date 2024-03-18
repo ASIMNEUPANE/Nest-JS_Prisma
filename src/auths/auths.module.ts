eceddd3d634223eb7a2b513fe05d562b714ae213
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-18 20:29:31 +0545

    complete register api

--- src/auths/auths.module.ts
+++ src/auths/auths.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthsService } from './auths.service';
 import { AuthsController } from './auths.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { BcryptPass } from 'src/utils/Bcrypt';
 
 @Module({
   imports: [PrismaModule],
 
   controllers: [AuthsController],
-  providers: [AuthsService],
+  providers: [AuthsService,BcryptPass],
 })
 export class AuthsModule {}
--- src/auths/auths.service.ts
+++ src/auths/auths.service.ts
@@ -3,12 +3,30 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma } from '@prisma/client';
-
+import { BcryptPass } from 'src/utils/Bcrypt';
+import { generateOTP } from 'src/utils/otp';
+import { mailer } from 'src/utils/mailer';
 @Injectable()
 export class AuthsService {
-  constructor(private prisma: PrismaService) {}
-  register(createAuthDto: Prisma.UserCreateInput) {
-    return this.prisma.user.create({ data: createAuthDto });
+  constructor(
+    private prisma: PrismaService,
+    private  bcrypt: BcryptPass,
+  ) {}
+
+  async register(createAuthDto: Prisma.UserCreateInput) {
+    let { isActive, isEmailVerified, roles, password, ...rest } = createAuthDto as {
+      password: string;
+      [key: string]: any;
+    };
+    rest.password = await this.bcrypt.hashPassword(password);
+    const user = await this.prisma.user.create({
+      data: rest as Prisma.UserUncheckedCreateInput,
+    });
+    const token = generateOTP();
+    const authUSer = { email: user.email, otp: token }; 
+    await this.prisma.auth.create({ data: authUSer });
+    await mailer(user?.email, token);
+    return user;
   }
 
   findAll() {
