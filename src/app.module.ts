b56f21d3046fea5702db26fa0a6798bc6313c372
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-18 11:07:15 +0545

    user services completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { UserModule } from './users/user.module';
+import { BlogModule } from './blog/blog.module';
+import { BlogModule } from './blog/blog.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BlogModule],
   controllers: [],
   providers: [],
 })
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { triggerAsyncId } from 'async_hooks';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BcryptPass } from 'utils/Bcrypt';
+import { CreateUserDto } from './dtos/CreateUser.dto';
+
 @Injectable()
 export class UserService {
   constructor(
@@ -11,7 +12,7 @@ export class UserService {
   ) {}
 
   async createUser(
-    payload: Prisma.UserCreateInput,
+    payload: CreateUserDto,
   ): Promise<Prisma.UserCreateInput | null> {
     let { password, ...rest } = payload as {
       password: string;
@@ -27,15 +28,77 @@ export class UserService {
     });
     return result;
   }
-  getUser() {
-    return this.prisma.user.findMany({
+  async getUser() {
+    return await this.prisma.user.findMany({
       where: { isActive: true, isArchive: false },
     });
   }
-  getById(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+  async getById(id: number) {
+    return await this.prisma.user.findUnique({ where: { id } });
   }
-  updateById(id:number, payload:Prisma.UserCreateInput){
+  async updateById(id:number, payload:Prisma.UserUpdateInput) {
+    return await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name: payload?.name,
+        images: payload?.images,
+      },
+    });
+  }
 
+  async changePassword(id: number, oldPassword: string, newPassword: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    const isValidOldPass = await this.bcrpt.comparePasswords(
+      oldPassword,
+      user?.password,
+    );
+    if (!isValidOldPass)
+      throw new HttpException(
+        'oldPassword is incorrect',
+        HttpStatus.BAD_REQUEST,
+      );
+    const newPass = await this.bcrpt.hashPassword(newPassword);
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: { password: newPass },
+    });
+  }
+
+  async resetPassword(id: number, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+
+    const newPass = await this.bcrpt.hashPassword(password);
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: { password: newPass },
+    });
+  }
+  async block(id: number, payload: { isActive: boolean }) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: { isActive: payload?.isActive },
+    });
+  }
+  async archive(id: number, payload: { isArchive: boolean }) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: { isArchive: payload?.isArchive },
+    });
   }
 }
