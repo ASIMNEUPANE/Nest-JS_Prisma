d62867dc9f13dda213d9b1579fcbb64d4ec3a8b6
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-21 17:43:08 +0545

    Refactor blog controller to include file upload and authorization

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,43 +1,72 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe,UploadedFile, UseGuards, Request } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
-import { UpdateBlogDto } from './dto/update-blog.dto';
 import { BlogEntity } from './entities/blog.entity';
 
-@Controller('blog')
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { RoleGuard } from 'src/auths/guards/role.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+
+
+@ApiBearerAuth('access-token')
+@UseGuards(RoleGuard)
+@Controller('blogs')
+@ApiTags('Blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
-  // @Post()
-  // create(@Body() createBlogDto: CreateBlogDto) {
-  //   return this.blogService.create(createBlogDto);
-  // }
 
-  @Get()
-  findAll() {
-    return this.blogService.findAll();
-  }
+  
+  @Post()
+  @Roles('ADMIN')
+  @ApiOperation({ summary: 'Create new Blog' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: [BlogEntity],
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @UseInterceptors(
+    FileInterceptor('images', {
+      storage: diskStorage({
+        destination: './public/blog',
+        filename: (req, file, cb) => {
+          // Generating a unique filename
+          const uniqueSuffix =
+            'blog'+Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const extension = file.originalname.split('.').pop();
+          cb(null, `${uniqueSuffix}.${extension}`);
+        },
+      }),
+    }),
+  )
+  registerUser(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+          new MaxFileSizeValidator({ maxSize: 100000 }),
+        ],
+        fileIsRequired: false,
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() createBlogDto: CreateBlogDto,
+    @Request() req:any,
+  ) {
+    if (file) {
+      const uniqueSuffix = Date.now() + '.' + file.originalname.split('.')[1];
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.blogService.findOne(+id);
-  }
+      createBlogDto.images = uniqueSuffix;
+    }
+createBlogDto.author = req.currentUserName;
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
-    return this.blogService.update(+id, updateBlogDto);
-  }
+console.log(createBlogDto)
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.blogService.remove(+id);
+    return this.blogService.create(createBlogDto);
   }
 }
+
+
