b5dfd7bef0d87b4890df8caf3b15f895b6e61299
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-21 17:43:45 +0545

    Add validation and API documentation to CreateBlogDto

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,33 +1,67 @@
 import { Category, Status, User } from '@prisma/client';
-import { IsNotEmpty, IsString, IsOptional, IsInt } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsInt, IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBlogDto {
   @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Full title of the blog',
+    example: 'Nepal the great',
+  })
   title: string;
 
   @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'content of the blog',
+    example: 'Nepal the great',
+  })
   content: string;
 
   @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'description of the blog',
+    example: 'Nepal the great',
+  })
   description: string;
 
   @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'category of the blog',
+    example: 'Nepal the great',
+  })
   category: Category;
 
-  @IsString()
-  @IsOptional()
-  status?: Status;
-
   @IsInt()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'total word of the blog',
+    example: 'Nepal the great',
+  })
   totalWord: number;
 
   @IsString()
+  @ApiProperty({
+    description: 'status of the blog',
+    example: 'Nepal the great',
+  })
+  
+  @IsOptional()
+  status?: Status;
+
+  @IsString()
+  @ApiProperty({
+    description: 'images of the blog',
+    example: 'Nepal the great',
+  })
   @IsOptional()
   images?: string;
 
-  
-  created_By: User;
-
-  @IsString()
+  @IsOptional()
   author: string;
+
+
 }
