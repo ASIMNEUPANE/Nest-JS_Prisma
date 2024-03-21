fc19b6db30e53c00bbb45bd300b28bfa7aec3702
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-21 17:43:23 +0545

    Refactor BlogService findAll method to support pagination and search

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -4,27 +4,57 @@ import { UpdateBlogDto } from './dto/update-blog.dto';
 import { Prisma } from '@prisma/client';
 import { BlogEntity } from './entities/blog.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Iblog, getReturn } from './blog.type';
+
 @Injectable()
 export class BlogService {
   constructor(private prisma: PrismaService) {}
 
-  // async create(createBlogDto: CreateBlogDto) {
-  //   return await this.prisma.blog.create({ data: createBlogDto  });
+  async create(createBlogDto: CreateBlogDto) {
+    return await this.prisma.blog.create({ data: createBlogDto  });
+  }
+
+  async findAll( limit: number,
+    page: number,
+    search: { title?: string; author?: string }):Promise<getReturn> {
+      const pageNum = page || 1;
+      const size = limit || 4;
+    
+      const whereCondition: any = {
+        status: "Published",
+      };  
+      if (search.title) {
+        whereCondition.title = search.title;
+      }
+    
+      if (search.author) {
+        whereCondition.author = search.author;
+      }
+    
+        // Get total count
+        const total = await this.prisma.blog.count({
+          where: whereCondition
+        });
+    
+        // Fetch paginated data
+        const data = await this.prisma.blog.findMany({
+          
+            where: whereCondition,
+          skip: (pageNum - 1) * size,
+          take: size,
+        });
+    
+        return { data, total, limit: size, page: pageNum };
+      }
+
+  // findOne(id: number):Promise<Iblog> {
+  //   return this.prisma.blog.findUnique({ where: { id } });  }
+
+  // update(id: number, updateBlogDto: UpdateBlogDto) {
+  //   return `This action updates a #${id} blog`;
   // }
 
-  findAll() {
-    return `This action returns all blog`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} blog`;
-  }
-
-  update(id: number, updateBlogDto: UpdateBlogDto) {
-    return `This action updates a #${id} blog`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} blog`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} blog`;
+  // }
 }
