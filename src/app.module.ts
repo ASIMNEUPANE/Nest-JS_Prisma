80a97d250ee1a9fbe4580fd6be387941f1764d15
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-21 17:41:46 +0545

    Add BlogModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,10 @@ import { AuthsModule } from './auths/auths.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RoleGuard } from './auths/guards/role.guard';
 import { PrismaService } from './prisma/prisma.service';
-// import { BlogModule } from './blog/blog.module';
-// import { BlogModule } from './blog/blog.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [UserModule, AuthsModule],
+  imports: [UserModule, AuthsModule,BlogModule],
   controllers: [],
   providers: [
     { provide: APP_GUARD, useClass: RoleGuard },
