f5ab7e90eeb8474369ee56a819e332c02858b069
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-22 11:51:55 +0545

    Add Get endpoint to retrieve all blogs

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Post, Body, UseInterceptors, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe,UploadedFile, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseInterceptors,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  UploadedFile,
+  UseGuards,
+  Request,
+  Get,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { BlogService } from './blog.service';
-import { CreateBlogDto } from './dto/create-blog.dto';
+import { CreateBlogDto, GetBlogDto } from './dto/create-blog.dto';
 import { BlogEntity } from './entities/blog.entity';
 
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { RoleGuard } from 'src/auths/guards/role.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
-
 
 @ApiBearerAuth('access-token')
 @UseGuards(RoleGuard)
@@ -17,8 +35,6 @@ import { Roles } from 'src/common/decorators/roles.decorator';
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
-
-  
   @Post()
   @Roles('ADMIN')
   @ApiOperation({ summary: 'Create new Blog' })
@@ -35,7 +51,7 @@ export class BlogController {
         filename: (req, file, cb) => {
           // Generating a unique filename
           const uniqueSuffix =
-            'blog'+Date.now() + '-' + Math.round(Math.random() * 1e9);
+            'blog' + Date.now() + '-' + Math.round(Math.random() * 1e9);
           const extension = file.originalname.split('.').pop();
           cb(null, `${uniqueSuffix}.${extension}`);
         },
@@ -54,19 +70,36 @@ export class BlogController {
     )
     file: Express.Multer.File,
     @Body() createBlogDto: CreateBlogDto,
-    @Request() req:any,
+    @Request() req: any,
   ) {
     if (file) {
       const uniqueSuffix = Date.now() + '.' + file.originalname.split('.')[1];
 
       createBlogDto.images = uniqueSuffix;
     }
-createBlogDto.author = req.currentUserName;
+    createBlogDto.author = req.currentUserName;
 
-console.log(createBlogDto)
+    console.log(createBlogDto);
 
     return this.blogService.create(createBlogDto);
   }
+
+  @Get()
+  @ApiOperation({ summary: 'List of all blog' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: [BlogEntity],
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  getBlogs(
+    @Query('limit', ParseIntPipe) limit: number,
+    @Query('page', ParseIntPipe) page: number,
+    @Body() getBlogDto: GetBlogDto,
+  ) {
+    const { title, author } = getBlogDto;
+    const search = { title, author };
+
+    return this.blogService.findAll(limit, page, search);
+  }
 }
-
-
