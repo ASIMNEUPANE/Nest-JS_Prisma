34b37bc37e0f5ff901a7aaff2897cf28eaecd633
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-22 11:52:25 +0545

    Refactor findAll method in BlogService

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -11,50 +11,55 @@ export class BlogService {
   constructor(private prisma: PrismaService) {}
 
   async create(createBlogDto: CreateBlogDto) {
-    return await this.prisma.blog.create({ data: createBlogDto  });
+    return await this.prisma.blog.create({ data: createBlogDto });
   }
 
-  async findAll( limit: number,
+  async findAll(
+    limit: number,
     page: number,
-    search: { title?: string; author?: string }):Promise<getReturn> {
-      const pageNum = page || 1;
-      const size = limit || 4;
-    
-      const whereCondition: any = {
-        status: "Published",
-      };  
-      if (search.title) {
-        whereCondition.title = search.title;
-      }
-    
-      if (search.author) {
-        whereCondition.author = search.author;
-      }
-    
-        // Get total count
-        const total = await this.prisma.blog.count({
-          where: whereCondition
-        });
-    
-        // Fetch paginated data
-        const data = await this.prisma.blog.findMany({
-          
-            where: whereCondition,
-          skip: (pageNum - 1) * size,
-          take: size,
-        });
-    
-        return { data, total, limit: size, page: pageNum };
-      }
+    search: { title?: string; author?: string },
+  ): Promise<getReturn> {
+    const pageNum = page || 1;
+    const size = limit || 4;
 
-  // findOne(id: number):Promise<Iblog> {
-  //   return this.prisma.blog.findUnique({ where: { id } });  }
+    const whereCondition: any = {
+      status: 'Published',
+    };
+    if (search.title) {
+      whereCondition.title = search.title;
+    }
 
-  // update(id: number, updateBlogDto: UpdateBlogDto) {
-  //   return `This action updates a #${id} blog`;
-  // }
+    if (search.author) {
+      whereCondition.author = search.author;
+    }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} blog`;
-  // }
+    // Get total count
+    const total = await this.prisma.blog.count({
+      where: whereCondition,
+    });
+
+    // Fetch paginated data
+    const data = await this.prisma.blog.findMany({
+      where: whereCondition,
+      skip: (pageNum - 1) * size,
+      take: size,
+    });
+
+    return { data, total, limit: size, page: pageNum };
+  }
+
+  async findOne(id: number): Promise<Iblog> {
+    return this.prisma.blog.findUnique({ where: { id } });
+  }
+
+  async update(id: number, updateBlogDto: UpdateBlogDto): Promise<Iblog> {
+    return await this.prisma.blog.update({
+      where: { id },
+      data: updateBlogDto,
+    });
+  }
+
+  async deleteById(id: number) {
+    return await this.prisma.blog.delete({ where: { id } });
+  }
 }
