3786e7736c7d9c785354e688484d6abd7837a3db
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-03 12:56:53 +0545

    Add useLogin hook for authentication

--- client/hooks/useAuth.tsx
+++ client/hooks/useAuth.tsx
@@ -1,6 +1,28 @@
+import { login } from "@/services/auth"
+import { UserStore } from "@/store/UserStore"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { useEffect } from "react"
 
-function Login() {
- 
+const useLogin = (email: string, password: string) => {
+    console.log(email, password, 'hoooooooksssssss')
+    const { setIsLoggedIn } = UserStore((state) => state)
+
+    const mutation = useMutation({
+        mutationFn: async () => {
+
+            const {data} = await login({ email, password })
+            console.log(data, '==============')
+            return data;
+        },
+
+    })
+
+        useEffect(() => {
+            if (mutation.isSuccess) setIsLoggedIn(mutation.data)
+        }, [mutation.data, setIsLoggedIn])
+    return mutation
 }
 
-export default Login
+
+
+export default useLogin
