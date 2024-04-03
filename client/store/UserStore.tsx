bfe8ee3e9e9a419c00b65168ee06a2560f7f84e9
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-03 12:57:14 +0545

    Refactor UserStore to include user authentication and roles

--- client/store/UserStore.tsx
+++ client/store/UserStore.tsx
@@ -1,15 +1,34 @@
 import { create } from 'zustand'
 
+type user = {
+    name: string,
+    email: string,
+    password: string,
+    images?: string,
+    roles?: string[],
 
-const user = {
-    user: {},
+}
+type UserStateType = {
+    user: user | null,
+    isLoggedIn: boolean,
+    roles: string[],
+}
+type UserActionType = {
+    setIsLoggedIn: (payload: user) => void,
+    setLogOut: () => void,
+
+}
+type UserStoreType = UserStateType & UserActionType
+const initialUserState: UserStateType = {
+    user: null,
     isLoggedIn: false,
     roles: [],
-}
-const UserStore = create((set) => ({
-    user,
- 
+};
 
-    setIsloggedIn: () => set((state: any) => ({ sta: state.payload }))
+export const UserStore = create<UserStoreType>((set) => ({
+    ...initialUserState,
+
+    setIsLoggedIn: (payload: user) => set((state: any) => ({ ...state, isLoggedIn: true, user: payload, roles: payload.roles })),
+    setLogOut: () => set(initialUserState), // Reset to initial state on logout
 }
 ))
