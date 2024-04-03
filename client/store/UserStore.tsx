e1bc726680480da8903e82fae37f1f66101a2d61
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-03 16:41:46 +0545

    Refactor UserStore.tsx to use TypeScript interfaces

--- client/store/UserStore.tsx
+++ client/store/UserStore.tsx
@@ -1,34 +1,38 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
-type user = {
-    name: string,
-    email: string,
-    password: string,
-    images?: string,
-    roles?: string[],
+type User = {
+    name: string;
+    email: string;
+    password: string;
+    images?: string;
+    roles?: string[];
+};
 
-}
 type UserStateType = {
-    user: user | null,
-    isLoggedIn: boolean,
-    roles: string[],
-}
-type UserActionType = {
-    setIsLoggedIn: (payload: user) => void,
-    setLogOut: () => void,
+    user: User | null;
+    isLoggedIn: boolean;
+    roles: string[];
+};
 
-}
-type UserStoreType = UserStateType & UserActionType
+type UserActionType = {
+    setIsLoggedIn: (payload: { user: User }) => void;
+    setLogOut: () => void;
+};
+
+type UserStoreType = UserStateType & UserActionType;
+
 const initialUserState: UserStateType = {
     user: null,
     isLoggedIn: false,
     roles: [],
 };
 
+console.log(initialUserState, 'initialUserState');
+
 export const UserStore = create<UserStoreType>((set) => ({
     ...initialUserState,
 
-    setIsLoggedIn: (payload: user) => set((state: any) => ({ ...state, isLoggedIn: true, user: payload, roles: payload.roles })),
+    setIsLoggedIn: (payload: { user: User }) => set((state: any) =>
+        ({ ...state, isLoggedIn: true, user: payload.user, roles: payload.user.roles })),
     setLogOut: () => set(initialUserState), // Reset to initial state on logout
-}
-))
+}));
