0c1d63379a6e77f812172413c041dc4765d2a264
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-09 23:35:21 +0545

    Add sign-up form validation and submit functionality

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,30 +1,59 @@
 import Link from "next/link"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import { z } from "zod"
 
+
+const signUpValidation = z.object({
+    name: z.string().min(1),
+
+    email: z
+        .string()
+        .min(1, { message: "This field has to be filled." })
+        .email("This is not a valid email."),
+    password: z
+        .string()
+        .min(8, { message: "Too short" })
+        .regex(/[A-Z]/, { message: "Must contain at least one uppercase letter" })
+        .regex(/[a-z]/, { message: "Must contain at least one lowercase letter" })
+        .regex(/[0-9]/, { message: "Must contain at least one numeric digit" })
+        .regex(/[!@#$%^&*]/, {
+            message: "Must contain at least one special character",
+        }),
+
+})
+type SignIn = z.infer<typeof signUpValidation>;
+
+const [user, setUser] = useState<SignIn>({
+    name: '',
+    email: '',
+    password: ''
+})
+const submit = ( ) => {
+
+    console.log('ajsd')
+}
 function page() {
     return (
-        <div className="m-28">
-            <div className="w-64 h-12 left-[83px] top-[134px] absolute text-center text-black text-2xl font-bold font-['Poppins']">Hi, Welcome Back! 👋</div>
-            <div className="w-80 h-20 left-[42px] top-[200px] absolute">
-                <input className="w-80 h-12 left-0 top-[27px] absolute bg-white bg-opacity-10 rounded-lg border border-black border-opacity-40" placeholder="example@gmail.com" />
-                <div className="w-14 h-5 left-[12px] top-0 absolute text-center text-stone-600 text-sm font-normal font-['Poppins']">Email</div>
+        <>
+            <div className="h-screen flex justify-center items-center ">
+                <div className="p-7 rounded bg-black">
+                    <label className="ml-1 text-white">username</label>
+                    <Input value={user.email}
+                        onChange={(e) => {
+                            setUser((prev) => {
+                                return { ...prev, email: e.target.value };
+                            });
+
+                        }} className="block mt-2 w-72 rounded-md  text-white" type="text" placeholder="Username" />
+                    <label className="ml-1 text-white">Email</label>
+                    <Input className="block mt-2 w-72 rounded-md  text-white" type="text" placeholder="Email" />
+                    <label className="ml-1 text-white">password</label>
+                    <Input className="block mt-2 w-72 rounded-md  text-white" type="text" placeholder="Password" />
+                    <Button className="mt-4 " onClick={submit}>Submit</Button>        </div>
             </div>
-            <div className="w-80 h-20 left-[42px] top-[295px] absolute">
-                <input className="w-80 h-12 left-0 top-[27px] absolute bg-white bg-opacity-10 rounded-lg border border-black border-opacity-40" placeholder="example@gmail.com" />
-                <div className="w-4 h-4 left-[279px] top-[43px] absolute" />
-                <div className="w-20 h-5 left-[15px] top-0 absolute text-center text-stone-600 text-sm font-normal font-['Poppins']">Password</div>
-            </div>
-            <div className="w-80 h-6 left-[46.20px] top-[393px] absolute">
-                <button className="left-[173.80px] top-0 absolute text-red-400 text-base font-medium font-['Poppins']">Forgot Password?</button>
-            </div>
-            <div className="w-80 h-12 left-[43px] top-[447px] absolute">
-                <div className="w-80 h-12 left-0 top-0 absolute bg-blue-700 rounded" />
-                <button className="left-[134px] top-[11px] absolute text-white text-opacity-90 text-base font-semibold font-['Poppins']">Login</button>
-            </div>
-            <div className="w-64 h-5 left-[67.68px] top-[526px] absolute">
-                <div className="w-48 h-5 left-0 top-0 absolute text-stone-950 text-base font-semibold font-['Manrope']">Don’t have an account ? </div>
-                <Link href={'login'} className="w-16 h-5 left-[199.83px] top-0 absolute text-violet-950 text-base font-semibold font-['Manrope']">Sign Up</Link>
-            </div>
-        </div>
+        </>
     )
 }
 
