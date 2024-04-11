f0bb0fc219fee794e091d7c28587f110d74f765f
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-11 10:33:56 +0545

    Update Navbar links

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -6,7 +6,8 @@ function Navbar() {
     <div className='flex gap-11 bg-slate-900 '>
         <div className=' rounded-sm m-4 hover:text-gray-300 text-red-50 px-3 py-2' ><Link href={'/'}>Home</Link></div>
         <div className=' rounded-sm m-4 hover:text-gray-300 text-red-50 px-3 py-2' ><Link href={'blog'}>Blog</Link></div>
-        <div className=' rounded-sm m-4 hover:text-gray-300 text-red-50 px-3 py-2' ><Link href={'login'}>SignIn</Link></div>
+        <div className=' rounded-sm m-4 hover:text-gray-300 text-red-50 px-3 py-2' ><Link href={'login'}>LogIn</Link></div>
+        <div className=' rounded-sm m-4 hover:text-gray-300 text-red-50 px-3 py-2' ><Link href={'signup'}>SignUp</Link></div>
         
     </div>
   )
