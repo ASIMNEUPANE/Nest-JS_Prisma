e3653d7db76bb8d95f14fdc2f5ec61e5871a2319
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-11 11:48:12 +0545

    Refactor usePost hook to handle success state

--- client/hooks/usePost.tsx
+++ client/hooks/usePost.tsx
@@ -2,6 +2,7 @@ import API from "@/utils/API"
 import { useMutation } from "@tanstack/react-query"
 import { useState } from "react"
 import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { boolean } from "zod"
 
 // Get QueryClient from the context
 type loginPayload = {
@@ -10,7 +11,7 @@ type loginPayload = {
 }
 
 
-const usePost = (qkey:string) => {
+const usePost = (qkey: string) => {
     const queryClient = useQueryClient()
 
     // const mutations = useMutation({
@@ -22,27 +23,31 @@ const usePost = (qkey:string) => {
 
 
     //     }
-    const [error, setError] = useState()
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
     const { mutate: postMutation, isError, isSuccess, data } = useMutation({
 
         mutationFn: async (payload: any) => {
             try {
-                const { data } = await API.post(payload.urls, { ...payload.payload });
-                console.log(data)
+                console.log(payload, 'payload')
+                const { data } = await API.post(payload.urls, { ...payload.data });
+                setSuccess(true)
                 return data;
             }
             catch (error) {
+                setSuccess(false)
                 setError((error as any).response.data.message)
             }
 
         },
         onSuccess: async () => {
-            await queryClient.invalidateQueries({ queryKey: [qkey] });
-          },
+            if (qkey != 'false')
+                await queryClient.invalidateQueries({ queryKey: [qkey] });
+        },
 
     })
     console.log(data)
-    return { postMutation, isError, isSuccess, data, error }
+    return { postMutation, isError, isSuccess, data, error, success }
 }
 
 export default usePost;
