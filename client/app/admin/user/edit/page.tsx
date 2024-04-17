fbde6579d0238e7901890f8548e10b3e5a1f1879
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-17 11:41:08 +0545

    Add user add and edit pages, and update admin navbar

---
+++ client/app/admin/user/edit/page.tsx
@@ -0,0 +1,38 @@
+
+function edit() {
+    return (
+        <div className=" p-4 ">
+            <div className='flex justify-center items-center '>
+                <form className="  rounded-2xl space-y-6 text-white p-5 bg-gray-800 w-3/4">
+                    <div className=' pl-52 flex flex-col items-center justify-center w-3/5'>
+                    <div className='p-2'  >
+                        <div className='pr-2 font-semibold '>Name</div>
+                        <input className='bg-white text-black rounded-sm p-2 w-80' type="text" placeholder='Name' />
+                    </div>
+                    
+                    <div className='p-2' >
+                        <div className='pr-2 font-semibold'>Email</div>
+                        <input className='bg-white text-black rounded-sm p-2 w-80' type="email" placeholder='email' />
+                    </div>
+                    <div  className='p-2'>
+                        <div className='pr-2 font-semibold'>Password</div>
+                        <input className='bg-white text-black rounded-sm p-2 w-80' type="password" placeholder='password' />
+                    </div>
+                    <div  className='p-2'>
+                        <div className='pr-2 font-semibold'>images</div>
+                        <input className='bg-white text-black rounded-sm p-2 w-80' type="file" placeholder='images' />
+                    </div>
+                    <div  className='p-2'>
+
+                    <button type='submit' className=" p-3 w-80 bg-gradient-to-r from-blue-400 to-cyan-200  font-semibold rounded-full ">Update User</button>
+                    </div>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default edit
--- client/app/admin/user/page.tsx
+++ client/app/admin/user/page.tsx
@@ -1,9 +1,70 @@
+import Link from 'next/link'
 import React from 'react'
-
+import { MdBlock } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { GrEdit } from "react-icons/gr";
 function user() {
-  return (
-    <div>user</div>
-  )
+    return (
+
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-3/4 mx-auto mt-3">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                <thead className="text-xs text-white uppercase bg-black dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Roles
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            isActive
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            IsArchive
+                        </th>
+                        <th scope="col" className="px-6 py-3 bg-green-500  hover:bg-green-600  hover:focus-visible hover:underline ">
+                            <Link href={'/admin/user/add'}>
+                                Add
+                            </Link>
+                        </th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className=" text-white bg-gray-800 border-b dark:border-gray-700 hover:bg-gray-800 dark:hover:bg-gray-600">
+                        <th scope="row" className=" text-white px-6 py-4 font-medium   dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 text-right flex gap-2">
+                            <button><MdBlock color='black ' size={25} /></button>
+                            <button className=''><MdDelete color='red'  size={25} />
+                            </button>
+                            <Link href={'/admin/user/edit'} className="font-medium text-blue-600 dark:text-blue-500 hover:underline"><GrEdit  size={25} />
+                            </Link>
+                        </td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default user
