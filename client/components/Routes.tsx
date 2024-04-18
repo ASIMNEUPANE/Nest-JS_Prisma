bca1d38b80d7ecaa9615fe9b0483e7a168e51aab
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-18 14:50:03 +0545

    Add Routes component for handling private and admin routes

---
+++ client/components/Routes.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
+import { getToken } from "../utils/session";
+import moment from "moment";
+
+interface AdminRouteProps {
+  children: React.ReactNode;
+  role: string;
+}
+
+interface PrivateRouteProps {
+  children: React.ReactNode;
+}
+
+export const AdminRoute: React.FC<AdminRouteProps> = ({ children, role }) => {
+  const router = useRouter()
+  const isLoggedIn = checkIsLoggedIn();
+  const isAdminRole = isAdmin(role);
+
+  return (
+    <>
+      {isLoggedIn && isAdminRole ? (
+        children
+      ) : isLoggedIn && !isAdminRole ? (
+        router.push('/')
+      ) : (
+        router.push('/admin')
+              )}
+    </>
+  );
+};
+
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const isLoggedIn = checkIsLoggedIn();
+  const router = useRouter()
+
+
+  return <>{isLoggedIn ?  router.push('/admin') : children}</>;
+};
+
+const isAdmin = (role: string | undefined): boolean => {
+  if (!role) return true;
+  // CHECK JWT TOKEN (Private)
+  const token = getToken();
+  console.log(token)
+  if (!token) return false;
+  // check for access token duration
+  const data = jwtDecode (token as string);
+  console.log(data,'dataSSSSSSSSSSSSSSSSSSSSSS')
+  const isValid = data.roles.includes(role);
+  return isValid;
+};
+
+const checkIsLoggedIn = (): boolean => {
+  // check for access token
+  const token = getToken();
+  if (!token) return false;
+  // check for access token duration
+  const { exp } = jwtDecode (token as string);
+  const now = new Date().valueOf();
+  const isValid = moment.unix(exp as number) > moment(now);
+  return isValid;
+};
